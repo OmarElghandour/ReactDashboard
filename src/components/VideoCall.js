85fd1b111558222d157ecbdbe7ed888d952a00cf
omarelghandour <omarelghandoor@gmail.com>
2020-09-05 01:10:49 +0200

    add video call functionality

--- src/components/VideoCall.js
+++ src/components/VideoCall.js
@@ -1,83 +1,74 @@
 import * as React from 'react';
 import io from 'socket.io-client';
 import Peer from 'peerjs';
-import uuid from 'react-uuid';
-
 const socket = io('http://localhost:5000');
 const peer = new Peer();
-
 
 const VideoCall = (props) => {
     const {match: {params}} = props;
     const ROOM_ID = params.roomId;
-    const [currentSocketId , setCurrentSocketId] = React.useState('');
     const [videos, setVideos] = React.useState([]);
-    const [userSockets, setUserSockets] = React.useState(null);
-    React.useEffect(() => {
-        socket.on('all-users', users => {
-            console.log('all-users');
-            console.log(users);
-            setUserSockets(users);
+    const streamerVideo = React.useRef();
+    React.useEffect( () => {
+        socket.on('user-disconnect', user => {
+           const video = document.getElementById(user);
+           if (video){
+               video.remove();
+           }
         });
-
-        peer.on('open', userId => {
-            console.log('1st here is peer is open');
+       peer.on('open', userId => {
             socket.emit('join-room', ROOM_ID, userId);
-            setCurrentSocketId(userId);
         });
-
         navigator.mediaDevices.getUserMedia({
             video: true,
-            audio: false
+            audio: true
         }).then(stream => {
-            addVideoStream(stream , currentSocketId);
+            const myVideo = streamerVideo.current;
+            myVideo.srcObject = stream;
+            myVideo.muted = "muted";
             peer.on('call', call => {
                 call.answer(stream);
-                call.on('stream', (otherVideoStream) => {
-                    addVideoStream(otherVideoStream);
-                })
+                call.on('stream', (otherVideoStream ) => {
+                    addParticipants(call.peer , stream);
+                });
             });
-            socket.on('user-connected', userId => {
-                console.log('user-connected');
-                connectNewUser(userId, stream);
+            socket.on('user-connected', user => {
+                connectNewUser(user.userId, stream);
             });
         });
 
         const connectNewUser = (userId, stream) => {
             let call = peer.call(userId, stream);
             call.on('stream', function (userStream) {
-                addVideoStream(userStream , userId);
+                addParticipants(userId , userStream);
             });
         };
-
-        const addVideoStream = (stream , userId) => {
-            socket.on('user-connected', userId => {
-                console.log('user-connected');
-                console.log(userId);
-                // connectNewUser(userId, stream);
-            });
-
-            setVideos(oldArray => [...oldArray, {id: uuid(), stream: stream}]);
-        }
     }, []);
+    const addParticipants = (userId , stream) => {
+        let isExist = videos.find(x => x.id === userId);
+        if (!isExist){
+            setVideos(oldArray => [...oldArray, {id: userId, stream: stream}]);
+        }
+    };
+
     const handelVideo = (id, stream) => {
-        console.log(userSockets);
-        console.log(videos);
-        videos.forEach((videoObj , i) => {
+        videos.forEach(() => {
                 const video = document.getElementById(id);
                 video.srcObject = stream;
         });
     };
     return (
         <div>
+            <div className={'streamer'} >
+                <video autoPlay ref={streamerVideo} />
+            </div>
             {
                 videos.length > 0 ?
                     videos.map((vid) => {
                         return (
-                            <video key={vid.id} id={vid.id} ref={() => handelVideo(vid.id, vid.stream)}
+                            <video muted="muted" key={vid.id} id={vid.id} ref={() => handelVideo(vid.id, vid.stream)}
                                    playsInline={true} width={'400px'} height={'400px'} autoPlay/>
                         );
-
                     })
                     : "no stream"
             }
